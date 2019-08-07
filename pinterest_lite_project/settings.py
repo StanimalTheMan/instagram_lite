e9a5bdc380e97325718941c9ddc0fe01410c8471
StanimalTheMan <sjc591@nyu.edu>
2019-08-07 17:16:22 -0400

    started pages app...tests tbd

--- pinterest_lite_project/settings.py
+++ pinterest_lite_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Local
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'pinterest_lite_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
