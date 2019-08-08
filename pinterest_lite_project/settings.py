f90cfd758f94a4e4e980c12ff7a2abb59918dada
StanimalTheMan <sjc591@nyu.edu>
2019-08-08 01:50:40 -0400

    do i push environment variables config

--- pinterest_lite_project/settings.py
+++ pinterest_lite_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's0jc5pn0ypdgbqr13=8w#*x@5gw1dwt2k0kb2a$(=3b!rsf6_%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
