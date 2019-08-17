8f2c7ce31a027c46daa1596b4bc3ba65f1a4ae62
StanimalTheMan <sjc591@nyu.edu>
2019-08-17 16:06:59 -0400

    added update and delete functionalites

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, re_path
 
 
-from .views import CreatePostView, PostDetailView
+from .views import CreatePostView, PostDetailView, PostUpdateView, PostDeleteView
 
 
 urlpatterns = [
     path('new/', CreatePostView.as_view(), name='add_post'),
     path('<uuid:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('<uuid:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('<uuid:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse,reverse_lazy
 from django.http import HttpResponseRedirect
 
@@ -9,10 +10,37 @@ from .models import Post
 from users.models import CustomUser
 
 # Create your views here.
+
+
 class CreatePostView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
     template_name = 'posts/post.html'
+    success_url = reverse_lazy('view_profile')
+
+    
+    def get_success_url(self):
+        return reverse('view_profile', kwargs={'username': self.request.user.username})
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save()
+        return HttpResponseRedirect(self.get_success_url())
+    
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'posts/post_detail.html'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('title', 'picture',)
+    template_name = 'posts/post_edit.html'
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'posts/post_delete.html'
     success_url = reverse_lazy('view_profile')
 
     def get_success_url(self):
@@ -23,8 +51,3 @@ class CreatePostView(LoginRequiredMixin, CreateView):
         obj.author = self.request.user
         obj.save()
         return HttpResponseRedirect(self.get_success_url())
-
-class PostDetailView(DetailView):
-    model = Post
-    context_object_name = 'post'
-    template_name = 'posts/post_detail.html'
